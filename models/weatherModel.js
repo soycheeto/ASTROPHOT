2a4bf5dc410e2b465c6c5d33d9c148d908d64641
soycheeto <trishahansen.ihs@gmail.com>
2024-05-18 19:10:43 +0530

    add css

--- models/weatherModel.js
+++ models/weatherModel.js
@@ -1,52 +1,40 @@
 const axios = require('axios');
 
 const getWeatherData = async (lat, lon, apiKey) => {
-  try {
-    const response = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
-      params: {
-        lat: lat,
-        lon: lon,
-        appid: apiKey,
-        units: 'metric'
-      }
-    });
-
-    return response.data;
-  } catch (error) {
-    throw new Error('Error fetching weather data');
-  }
+  const baseUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  const response = await axios.get(baseUrl);
+  return response.data;
 };
 
-const analyzeWeatherData = (weatherData) => {
-  const { temp, feels_like, humidity, visibility } = weatherData.main;
-  const { description, icon } = weatherData.weather[0];
-  const { sunrise, sunset } = weatherData.sys;
-  const place = weatherData.name;
-  const cloudCover = weatherData.clouds.all;
+const analyzeWeatherData = (data) => {
+  const { temp, feels_like, humidity } = data.main;
+  const visibility = data.visibility; // Extract visibility correctly
+  const place = data.name;
+  const { description, icon } = data.weather[0];
+  const { sunrise, sunset } = data.sys;
 
+  const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
   const fahrenheit = (temp * 9) / 5 + 32;
+
   const sunriseGMT = new Date(sunrise * 1000);
   const sunsetGMT = new Date(sunset * 1000);
 
-  const isOptimal = cloudCover < 30 && visibility > 8000 && humidity < 70;
-
-  const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  // Analyze weather conditions for optimal sky-viewing
+  const isOptimal = visibility > 9500 && humidity < 80; // Example criteria
 
   return {
-    isOptimal,
     weatherData: {
-      temp: temp.toFixed(2),
-      feels_like: feels_like.toFixed(2),
-      humidity,
-      visibility,
+      temp,
+      feels_like,
+      place,
       description,
       iconUrl,
-      place,
+      fahrenheit: fahrenheit.toFixed(2),
       sunrise: `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`,
       sunset: `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`,
-      fahrenheit: fahrenheit.toFixed(2),
-      cloudCover
-    }
+      visibility: visibility !== undefined ? visibility : 'No data available',
+    },
+    isOptimal,
   };
 };
 
