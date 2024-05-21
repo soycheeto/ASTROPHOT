ce0c480221da48fe05339d1cd4d585a11dedf2b4
soycheeto <trishahansen.ihs@gmail.com>
2024-05-21 16:10:18 +0530

    added comments

--- app.js
+++ app.js
@@ -4,18 +4,28 @@ const weatherRoutes = require('./routes/weatherRoutes');
 
 const app = express();
 
-// Set view engine
+// use EJS as the templating engine for rendering views
 app.set('view engine', 'ejs');
+
+// setting directory where view files are located by creating an absolute path with root dir and views dir
 app.set('views', path.join(__dirname, 'views'));
 
+
+//middleware functions are funcs that have access to the program's req, res and the next middleware in the req-res cycle
+
 // Serve static files from the 'public' directory
+//static() is a middleware function from the express module
+// any file in the 'public' dir can be accessed by the client via a url
+// static files do not need to be processed
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use weather routes
+// mounts the weatherRoutes middleware on the root path `/`
+// any requests to paths starting with `/` will by hanled by weatherRoutes middleware
+// weatherRoutes module defines routing logic for handling different endpoints of weather data
 app.use('/', weatherRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
