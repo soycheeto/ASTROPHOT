d46a53d6ab0965761cb9da19aa9b23de330dd0e1
soycheeto <trishahansen.ihs@gmail.com>
2024-05-31 23:04:25 +0530

    port 3000

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', weatherRoutes);
 
 // Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log(`Server is running on port ${3000}`);
 });
